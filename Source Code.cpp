ff9524b3d428dbc11e3d53e2ae9665e53f9047d2
Iftitahazzahrah <99011949+Iftitahazzahrah@users.noreply.github.com>
2022-02-06 21:46:57 +0700

    Add files via upload

---
+++ Source Code.cpp
@@ -0,0 +1,20 @@
+#include <iosterm>
+using namespace std;
+int main()
+{
+	float L, p, l;
+	
+	
+	cout << "=================================\n";
+	cout << "Program Menghitung Luas Persegi Panjang\n";
+	cout << "=================================\n";
+	cout << "Masukkan nilai panjang =";
+	cin >> p;
+	cout << "Masukkan nilai lebar =";
+	cin >> l;
+	L = p*l
+	cout << "Luas Persegi Panjang =" << (L) << end;
+	
+	
+	return 0;
+}
